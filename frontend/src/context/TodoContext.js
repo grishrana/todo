781c001087e0d97a12369e261917bea90f0cc0ed
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-10-04 13:03:10 +0545

    feat: add todo context provider

---
+++ frontend/src/context/TodoContext.js
@@ -0,0 +1,17 @@
+import { data } from "@/constant";
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: data,
+    showTasks: ()=> {},
+    showTask: ()=> {},
+    createTask: ()=> {},
+    updateTask: ()=> {},
+    deleteTask: ()=> {}
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+export const TodoContextProvider = TodoContext.Provider();
