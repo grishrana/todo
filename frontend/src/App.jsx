47d3342d9f2e80f5369ad219eba84115dc5225c0
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-10-06 15:47:57 +0545

    fix: manage imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import TodoItems from "./components/TodoItems"
+import CreateTodoDialog from "./components/create-todo/CreateTodoDialog"
+import { TodoContextProvider, useTodo } from "./context/TodoContext"
+import ShowTodo from "./components/ui/ShowTodo"
+import CreateTodo from "./components/create-todo/AddTask"
+import UpdateTodo from "./components/update-todo/UpdateTaskIcon"
+import DeleteTodo from "./components/ui/DeleteTodo"
 
 function App() {
+  const { todos } = useTodo();
 
   return (
-    <>
-    <h1>yo yo</h1>
-    </>
+    <TodoContextProvider value={{todos, ShowTodo, CreateTodo, UpdateTodo, DeleteTodo}}>
+      <div className="p-2">
+        <TodoItems data={todos} />
+        <CreateTodoDialog />
+      </div>
+    </TodoContextProvider>
   )}
 
 export default App
