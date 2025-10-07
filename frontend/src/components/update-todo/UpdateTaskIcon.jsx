1cc23e705b4c41921660906039f89de02790670c
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-10-07 06:39:41 +0545

    feat: forward reference for dialog children

--- frontend/src/components/update-todo/UpdateTaskIcon.jsx
+++ frontend/src/components/update-todo/UpdateTaskIcon.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import editIcon from "../../assets/edit-icon.svg";
 
-const UpdateTaskIcon = React.forwardRef((props, ref)=> {
+const UpdateTaskIcon =((props, ref)=> {
   return (
     <button ref={ref} className="cursor-pointer hover:bg-[#0f172a] hover:rounded-lg hover:scale-111" {...props}>
       <img src={editIcon} alt="edit" className="w-10"/>
