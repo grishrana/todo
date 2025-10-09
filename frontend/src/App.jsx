f7a774683ce15d8b3d66c703ca84378c8d4b2b88
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-10-09 10:44:20 +0545

    fix: isolate context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import TodoItems from "./components/TodoItems"
 import CreateTodoDialog from "./components/create-todo/CreateTodoDialog"
-import { TodoContextProvider, useTodo } from "./context/TodoContext"
 import ShowTodo from "./components/ui/ShowTodo"
-import CreateTodo from "./components/create-todo/AddTask"
-import UpdateTodo from "./components/update-todo/UpdateTaskIcon"
-import DeleteTodo from "./components/ui/DeleteTodo"
+import { useTodo } from "./hooks/useTodo"
+import TodoContextProvider from "./context/TodoContextProvider"
+
+function AppContent() {
+  const {todos} = useTodo();
+  return (
+    <div className="p-2">
+      <TodoItems data={todos} />
+      <CreateTodoDialog />
+    </div>
+  );
+}
 
 function App() {
-  const { todos } = useTodo();
-
   return (
-    <TodoContextProvider value={{todos, ShowTodo, CreateTodo, UpdateTodo, DeleteTodo}}>
-      <div className="p-2">
-        <TodoItems data={todos} />
-        <CreateTodoDialog />
-      </div>
+    <TodoContextProvider>
+      <AppContent />
     </TodoContextProvider>
   )}
 
